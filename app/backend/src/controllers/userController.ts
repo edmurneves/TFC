0e71893b315d3de3e84933c5c881353a1948b565
Edmur <edmurneves@gmail.com>
2022-07-29 20:04:47 -0300

    Criada a classe controler com o método login.

---
+++ app/backend/src/controllers/userController.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from 'express';
+import { IUserService } from '../interfaces';
+
+export default class Controller {
+  constructor(private service: IUserService) { // Inversão de dependência "D" do SOLID
+    this.service = service;
+  }
+
+  async login(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { email, password } = req.body;
+      const token = await this.service.login(email, password);
+      if (!token) return res.status(401).json({ message: 'Incorrect email or password' });
+      return res.status(200).json({ token });
+    } catch (error) {
+      next(error);
+    }
+  }
+}
