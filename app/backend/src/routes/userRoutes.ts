e9d8b216896101af367f85a59af5b92dc3e14283
Edmur <edmurneves@gmail.com>
2022-07-29 20:46:16 -0300

    Adicionado os middlewares de validação de e-mail e senha na rota user.

--- app/backend/src/routes/userRoutes.ts
+++ app/backend/src/routes/userRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
+import { checkEmail, checkPassword } from '../middlewares/checkLogin';
 import { UserFactory } from '../factory';
 
 const userRouter = Router();
 
-userRouter.post('/login', (req, res, next) => {
+userRouter.post('/login', checkEmail, checkPassword, (req, res, next) => {
   UserFactory().login(req, res, next);
 });
 
--- app/backend/src/utils/generateJWT.ts
+++ app/backend/src/utils/generateJWT.ts
@@ -3,12 +3,12 @@ import * as jwt from 'jsonwebtoken';
 import { IUser } from '../interfaces/index';
 import { SECRET, algorithm } from '../constants/index';
 
-function generateJWT(payload: Omit<IUser, 'password'>) {
+const generateJWT = (payload: Omit<IUser, 'password'>) => {
   const secret = process.env.JWT_SECRET || SECRET;
   const jwtConfig:object = {
     algorithm,
   };
   return jwt.sign({ payload }, secret, jwtConfig);
-}
+};
 
 export default generateJWT;
