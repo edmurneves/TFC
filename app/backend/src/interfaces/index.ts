ac3f41125f57abb9cbf91d0c425517b9fb700a05
Edmur <edmurneves@gmail.com>
2022-07-29 20:01:40 -0300

    Criação do arquivo com as interfaces utilizadas no código.

---
+++ app/backend/src/interfaces/index.ts
@@ -0,0 +1,15 @@
+export interface IUser {
+  id?: number;
+  username: string;
+  role: string;
+  email: string;
+  password: string;
+}
+
+export interface IUserModel {
+  findUser(email: string): Promise<IUser | null>;
+}
+
+export interface IUserService {
+  login(email: string, password: string): Promise<string | boolean>;
+}
