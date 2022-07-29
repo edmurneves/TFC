bfeadd6a731171c1506e5cd8dc1aaf14de2a3f2a
Edmur <edmurneves@gmail.com>
2022-07-29 20:03:18 -0300

    Criação do função de geração de token JWT.

---
+++ app/backend/src/utils/generateJWT.ts
@@ -0,0 +1,14 @@
+import 'dotenv/config';
+import * as jwt from 'jsonwebtoken';
+import { IUser } from '../interfaces/index';
+import { SECRET, algorithm } from '../constants/index';
+
+function generateJWT(payload: Omit<IUser, 'password'>) {
+  const secret = process.env.JWT_SECRET || SECRET;
+  const jwtConfig:object = {
+    algorithm,
+  };
+  return jwt.sign({ payload }, secret, jwtConfig);
+}
+
+export default generateJWT;
