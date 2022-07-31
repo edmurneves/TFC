98a391801de7e380cec248a1f1980716a8f503c5
Edmur <edmurneves@gmail.com>
2022-07-31 20:25:26 -0300

    Criado o factory de Team

--- app/backend/src/factory/index.ts
+++ app/backend/src/factory/index.ts
@@ -2,6 +2,10 @@ import UserRepository from '../repository/userRepository';
 import UserService from '../services/userService';
 import UserController from '../controllers/userController';
 
+import TeamRepository from '../repository/teamRepository';
+import TeamService from '../services/teamService';
+import TeamController from '../controllers/teamController';
+
 // Utilizando o Pattern Factory
 
 export const UserFactory = () => {
@@ -12,4 +16,10 @@ export const UserFactory = () => {
   return controller;
 };
 
-export default UserFactory;
+export const TeamFactory = () => {
+  const repository = new TeamRepository();
+  const service = new TeamService(repository);
+  const controller = new TeamController(service);
+
+  return controller;
+};
