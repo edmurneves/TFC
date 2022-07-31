a9360931bfffad9e5c25be84d1196203e2ca5d39
Edmur <edmurneves@gmail.com>
2022-07-31 18:24:09 -0300

    Criada a rota /login/validate

--- app/backend/src/routes/userRoutes.ts
+++ app/backend/src/routes/userRoutes.ts
@@ -8,4 +8,8 @@ userRouter.post('/login', checkEmail, checkPassword, (req, res, next) => {
   UserFactory().login(req, res, next);
 });
 
+userRouter.get('/login/validate', (req, res, next) => {
+  UserFactory().validateLogin(req, res, next);
+});
+
 export default userRouter;
