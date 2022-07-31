089ae610c3f30d8f924509cd1267e011d174e802
Edmur <edmurneves@gmail.com>
2022-07-31 20:55:26 -0300

    Adcionado o método getById ao TeamController

--- app/backend/src/controllers/teamController.ts
+++ app/backend/src/controllers/teamController.ts
@@ -14,4 +14,14 @@ export default class TeamController {
       next(error);
     }
   }
+
+  async getById(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { id } = req.params;
+      const team = await this.service.getById(id);
+      return res.status(200).json(team);
+    } catch (error) {
+      next(error);
+    }
+  }
 }
