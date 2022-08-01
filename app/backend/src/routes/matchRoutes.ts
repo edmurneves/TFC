90f3f39093758872fefcbe00d290d994774cf546
Edmur <edmurneves@gmail.com>
2022-08-01 20:37:39 -0300

    Criada a rota /matches

--- app/backend/src/routes/matchRoutes.ts
+++ app/backend/src/routes/matchRoutes.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
+import checkJWT from '../middlewares/checkJWT';
+import checkMatch from '../middlewares/checkMatch';
 import { MatchFactory } from '../factory';
 
 const matchRouter = Router();
 
 matchRouter.get('/matches', (req, res, next) => {
   MatchFactory().getAll(req, res, next);
+});
+
+matchRouter.post('/matches', checkJWT, checkMatch, (req, res, next) => {
+  MatchFactory().createMatch(req, res, next);
 });
 
 export default matchRouter;
