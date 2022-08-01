b5522cc80dbd765a836afd7b6ca9c0ae95c0921d
Edmur <edmurneves@gmail.com>
2022-08-01 20:34:45 -0300

    Criado o middleaware de validação de token.

---
+++ app/backend/src/middlewares/checkJWT.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from 'express';
+import 'dotenv/config';
+import * as jwt from 'jsonwebtoken';
+import { SECRET } from '../constants/index';
+
+const checkJWT = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const token = req.headers.authorization as string;
+    const JWT: string = process.env.JWT_SECRET || SECRET;
+    jwt.verify(token, JWT) as jwt.JwtPayload;
+    return next();
+  } catch (error) {
+    return res.status(401).json({ message: 'Token must be a valid token' });
+  }
+};
+
+export default checkJWT;
