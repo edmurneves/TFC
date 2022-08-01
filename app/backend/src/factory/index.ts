331c13e8433bf79d350c1a7f354343f48d19322b
Edmur <edmurneves@gmail.com>
2022-08-01 14:49:23 -0300

    Criado o factory de match

--- app/backend/src/factory/index.ts
+++ app/backend/src/factory/index.ts
@@ -6,6 +6,10 @@ import TeamRepository from '../repository/teamRepository';
 import TeamService from '../services/teamService';
 import TeamController from '../controllers/teamController';
 
+import MatchRepository from '../repository/matchRepository';
+import MatchService from '../services/matchService';
+import MatchController from '../controllers/matchController';
+
 // Utilizando o Pattern Factory
 
 export const UserFactory = () => {
@@ -20,6 +24,14 @@ export const TeamFactory = () => {
   const repository = new TeamRepository();
   const service = new TeamService(repository);
   const controller = new TeamController(service);
+
+  return controller;
+};
+
+export const MatchFactory = () => {
+  const repository = new MatchRepository();
+  const service = new MatchService(repository);
+  const controller = new MatchController(service);
 
   return controller;
 };
