84d6b8c44460fa7c380b279a9f3eabf8f831483a
Edmur <edmurneves@gmail.com>
2022-08-02 10:34:07 -0300

    Adicionado o test para checar a rota /login/validate

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -33,6 +33,10 @@ const { expect } = chai;
     password: '$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO'
   }
 
+  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7ImlkIjoyLCJ1c2VybmFtZSI6IlVzZXIiLCJyb2xlIjoidXNlciIsImVtYWlsIjoidXNlckB1c2VyLmNvbSIsInBhc3N3b3JkIjoiJDJhJDA4JFk4QWJpOGpYdnNYeXFtLnJtcDBCLnVRQkE1cVV6N1Q2R2hsZy9DdlZyL2dMeFlqNVVBWlZPIn0sImlhdCI6MTY1NzY1MjQ5OH0.UMEAZNmlMqHmvLDTrMz8dfxm1WrFfl07vn_hs2EI0_c'
+
+
+
 describe('Teste de login', () => {
     before(() => {
         sinon.stub(User, 'findOne').resolves(userMock as User)
@@ -62,6 +66,14 @@ describe('Teste de login', () => {
 
         expect(response.status).to.be.equal(400);
         expect(response.body).to.be.eql({ message: 'All fields must be filled'});
+    });
+
+    it('Testa a rota /login/validate caso sucesso retonar um objeto com a propriedade role.', async () => {
+      const response = await chai.request(app).get('/login/validate').set({ "Authorization": token });
+      // console.log(response.status);
+      // console.log(response.body);
+      expect(response.status).to.be.equal(200);
+      expect(response.body).have.property('role');
     });
 
 });
