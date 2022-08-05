0b818ae66cf3c75e6f07464fad79bf5d75386f90
Edmur <edmurneves@gmail.com>
2022-08-05 09:45:49 -0300

    Criação do método de calculo de vitórias, empates e derrotas.

--- app/backend/src/utils/businessRules.ts
+++ app/backend/src/utils/businessRules.ts
@@ -12,3 +12,16 @@ export const handleGoalsFavorOwnBalance = (matches: IMatch[]) => {
 
   return { goalsFavor, goalsOwn, goalsBalance };
 };
+
+export const handleVictoryDrawLoses = (matches: IMatch[]) => {
+  let totalVictories = 0;
+  let totalDraws = 0;
+  let totalLosses = 0;
+  matches.forEach((stats) => {
+    if (stats.homeTeamGoals - stats.awayTeamGoals > 0) totalVictories += 1;
+    else if (stats.homeTeamGoals - stats.awayTeamGoals < 0) totalLosses += 1;
+    else totalDraws += 1;
+  });
+
+  return { totalVictories, totalDraws, totalLosses };
+};
